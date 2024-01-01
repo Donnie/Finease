e3cd795cc430904e86894bb3da60f801ca3ab4d9
Donnie <donnie.ashok@moonfare.com>
2024-01-01 11:45:16 +0100

    v0.0.4 - Account Models

--- lib/db/migrations/a_initial_migration.dart
+++ lib/db/migrations/a_initial_migration.dart
@@ -4,25 +4,28 @@ Future<void> aInitialMigration(Database db) async {
   await db.execute('''
     CREATE TABLE Accounts (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      currency TEXT NOT NULL,
-      name TEXT NOT NULL,
-      type TEXT CHECK(type IN ('ASSET', 'LIABILITY', 'EQUITY', 'REVENUE', 'EXPENSE')),
-      balance BIGINT NOT NULL DEFAULT 0,
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      deleted_at DATETIME,
+      balance BIGINT NOT NULL DEFAULT 0,
+      currency TEXT NOT NULL,
+      liquid BOOLEAN,
+      name TEXT NOT NULL,
+      self BOOLEAN
     )
   ''');
 
   await db.execute('''
     CREATE TABLE Entries (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      deleted_at DATETIME,
       debit_account_id INTEGER NOT NULL,
       credit_account_id INTEGER NOT NULL,
       amount BIGINT NOT NULL,
+      date DATETIME,
       notes TEXT,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-      deleted_at DATETIME NULL,
       FOREIGN KEY (debit_account_id) REFERENCES Accounts(id),
       FOREIGN KEY (credit_account_id) REFERENCES Accounts(id)
     )
