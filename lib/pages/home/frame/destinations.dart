142f2f03c1b0785700ee507593a8391b95cd954f
Donnie <donnie.ashok@moonfare.com>
2024-01-06 10:39:11 +0100

    v0.0.8 - Rearrange

--- lib/pages/home/frame/destinations.dart
+++ lib/pages/home/frame/destinations.dart
@@ -1,8 +1,6 @@
-import 'package:finease/core/enum/page_type.dart';
 import 'package:finease/pages/home/summary/main.dart';
 import 'package:finease/pages/home/months/main.dart';
 import 'package:flutter/material.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 List<Destination> destinations = [
   Destination(
@@ -16,36 +14,6 @@ List<Destination> destinations = [
     pageType: PageType.months,
     icon: const Icon(Icons.credit_card_outlined),
     selectedIcon: const Icon(Icons.credit_card),
-  ),
-  Destination(
-    body: const SummaryPage(),
-    pageType: PageType.debts,
-    icon: Icon(MdiIcons.accountCashOutline),
-    selectedIcon: Icon(MdiIcons.accountCash),
-  ),
-  Destination(
-    body: const SummaryPage(),
-    pageType: PageType.overview,
-    icon: Icon(MdiIcons.sortVariant),
-    selectedIcon: Icon(MdiIcons.sortVariant),
-  ),
-  Destination(
-    body: const SummaryPage(),
-    pageType: PageType.categories,
-    icon: const Icon(Icons.category_outlined),
-    selectedIcon: const Icon(Icons.category),
-  ),
-  Destination(
-    body: const SummaryPage(),
-    pageType: PageType.budget,
-    icon: Icon(MdiIcons.timetable),
-    selectedIcon: Icon(MdiIcons.timetable),
-  ),
-  Destination(
-    body: const SummaryPage(),
-    pageType: PageType.recurring,
-    icon: Icon(MdiIcons.cashSync),
-    selectedIcon: Icon(MdiIcons.cashSync),
   ),
 ];
 
@@ -61,4 +29,12 @@ class Destination {
   final PageType pageType;
   final Icon selectedIcon;
   final Widget body;
+}
+
+enum PageType {
+  home,
+  months;
+
+  int get toIndex => index;
+  String get name => toString().split('.').last;
 }
