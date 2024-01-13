7d9d555870706e55a0350852ae5d2dd308b1fc24
Donnie Ashok <git@donnie.in>
2024-01-13 19:33:16 +0100

    v0.0.13 - Improve transaction UI (#12)
    * v0.0.13 - Delete transaction
    
    * v0.0.13 - Improve transaction UI
    
    * v0.0.13 - Refactor Past Account

---
+++ lib/pages/home/entries/entry_card.dart
@@ -0,0 +1,119 @@
+import 'package:finease/db/currency.dart';
+import 'package:finease/db/entries.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class EntryCard extends StatelessWidget {
+  final Entry entry;
+  final Function(int)? onDelete;
+
+  const EntryCard({
+    super.key,
+    required this.entry,
+    this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final String symbol = SupportedCurrency[entry.creditAccount!.currency]!;
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('From: ${entry.debitAccount!.name}'),
+                Text(getFormattedDate(entry.date!)),
+                Text('To: ${entry.creditAccount!.name}'),
+              ],
+            ),
+            Flex(
+              direction: Axis.horizontal,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '$symbol ${entry.amount}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      entry.notes!,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ],
+                ),
+                InkWell(
+                  onTap: () async {
+                    final bool confirm = await showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Confirm'),
+                          content: const Text(
+                              'Once you delete a transaction the balance in related accounts would be automatically updated'),
+                          actions: [
+                            TextButton(
+                              onPressed: () => Navigator.of(context).pop(false),
+                              child: const Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () => Navigator.of(context).pop(true),
+                              child: const Text('Delete'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+
+                    if (confirm) {
+                      onDelete?.call(entry.id!);
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 0,
+                    ),
+                    child: Icon(MdiIcons.delete),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+String getFormattedDate(DateTime entryDate) {
+  final now = DateTime.now();
+  final difference = now.difference(entryDate);
+
+  if (difference.inDays < 1) {
+    // Less than a day
+    if (difference.inHours < 1) {
+      // Less than a day
+      return 'moments ago';
+    }
+    return '${difference.inHours} hours ago';
+  } else if (difference.inDays < 7) {
+    // Less than a week
+    return '${difference.inDays} days ago';
+  } else {
+    // More than a week - show absolute time
+    return DateFormat('yyyy-MM-dd HH:mm').format(entryDate);
+  }
+}
