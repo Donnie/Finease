caf34a02abbc1c2b8c45f9e22af36eaa6aa7a4c7
Donnie <donnie.ashok@moonfare.com>
2024-01-14 14:15:16 +0100

    v0.0.14 - Export with Timestamp

--- lib/pages/settings/export_db.dart
+++ lib/pages/settings/export_db.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
 import 'package:finease/db/db.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:path/path.dart' as path; // Add this import for file path manipulation
 
 class ExportDatabaseWidget extends StatelessWidget {
   const ExportDatabaseWidget({super.key});
@@ -9,9 +12,17 @@ class ExportDatabaseWidget extends StatelessWidget {
   Future<bool> _exportDatabase() async {
     try {
       final databasePath = await DatabaseHelper().getDatabasePath();
-      final databaseFile = XFile(databasePath);
-      await Share.shareXFiles([databaseFile],
-          text: 'Here is my database file.');
+      final time = DateFormat('yyyyMMdd_HHmmss').format(DateTime.now());
+      final dir = path.dirname(databasePath);
+      final ext = path.extension(databasePath);
+      final newFilename = 'database_$time$ext';
+      final newPath = path.join(dir, newFilename);
+
+      final databaseFile = File(databasePath);
+      final newDatabaseFile = await databaseFile.copy(newPath);
+      final xFile = XFile(newDatabaseFile.path);
+
+      await Share.shareXFiles([xFile], text: 'Here is my database file.');
       return true;
     } catch (e) {
       return false;
