2041723eb82847ad561596c6851f6649a865984b
Donnie <donnie.ashok@moonfare.com>
2024-01-17 00:15:03 +0100

    v1.0.17 - Add indices

---
+++ lib/db/migrations/b_add_indices.dart
@@ -0,0 +1,25 @@
+import 'package:sqflite/sqflite.dart';
+
+Future<void> bAddIndices(Database db) async {
+  await db.execute('''
+    CREATE INDEX idx_accounts_id ON Accounts(id);
+    CREATE INDEX idx_accounts_balance ON Accounts(balance);
+    CREATE INDEX idx_accounts_currency ON Accounts(currency);
+    CREATE INDEX idx_accounts_liquid ON Accounts(liquid);
+    CREATE INDEX idx_accounts_hidden ON Accounts(hidden);
+    CREATE INDEX idx_accounts_name ON Accounts(name);
+    CREATE INDEX idx_accounts_type ON Accounts(type);
+
+    CREATE INDEX idx_entries_id ON Entries(id);
+    CREATE INDEX idx_entries_debit_account_id ON Entries(debit_account_id);
+    CREATE INDEX idx_entries_credit_account_id ON Entries(credit_account_id);
+    CREATE INDEX idx_entries_amount ON Entries(amount);
+    CREATE INDEX idx_entries_date ON Entries(date);
+    CREATE INDEX idx_entries_notes ON Entries(notes);
+
+    CREATE INDEX idx_settings_key ON Settings(key);
+
+    CREATE INDEX idx_migrations_id ON Migrations(id);
+  ''');
+}
+
