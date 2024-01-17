26281c93bb3722a58424901046c34238a977c0df
Donnie <donnie.ashok@moonfare.com>
2024-01-17 22:23:16 +0100

    v1.0.17 - Get getAllMonthsInsights

--- lib/db/months.dart
+++ lib/db/months.dart
@@ -6,41 +6,95 @@ class MonthService {
   MonthService({DatabaseHelper? databaseHelper})
       : _databaseHelper = databaseHelper ?? DatabaseHelper();
 
-  Future<List<Map<String, dynamic>>> getAllMonthsInsights() async {
+  Future<List<Month>> getAllMonthsInsights() async {
     final dbClient = await _databaseHelper.db;
-    final result = await dbClient.rawQuery('''
+    final results = await dbClient.rawQuery('''
       WITH RECURSIVE MonthDates(monthDate) AS (
-        SELECT DATETIME((SELECT MIN(date) FROM entries))
+        SELECT DATETIME((SELECT MIN(date) FROM entries), 'start of month')
         UNION ALL
         SELECT DATETIME(monthDate, '+1 month')
         FROM MonthDates
         WHERE DATETIME(monthDate, '+1 month') < CURRENT_DATE
+      ),
+      MonthlyTotals AS (
+        SELECT 
+          months.startDate,
+          months.endDate,
+          COALESCE((
+            SELECT
+              SUM(e.amount)
+            FROM entries AS e
+            LEFT JOIN accounts AS ac ON e.credit_account_id = ac.id
+            LEFT JOIN accounts AS ad ON e.debit_account_id = ad.id
+            WHERE
+              ac.type IN ('asset', 'liability') AND ad.type IN ('income', 'expense')
+              AND e.date BETWEEN months.startDate AND months.endDate
+              AND ac.currency = 'EUR'
+              AND ad.currency = 'EUR'
+          ), 0) AS income,
+          COALESCE((
+            SELECT
+              SUM(e.amount)
+            FROM entries AS e
+            LEFT JOIN accounts AS ac ON e.credit_account_id = ac.id
+            LEFT JOIN accounts AS ad ON e.debit_account_id = ad.id
+            WHERE
+              ad.type IN ('asset', 'liability') AND ac.type IN ('income', 'expense')
+              AND e.date BETWEEN months.startDate AND months.endDate
+              AND ac.currency = 'EUR'
+              AND ad.currency = 'EUR'
+          ), 0) AS expense
+        FROM (
+          SELECT 
+            REPLACE(DATETIME(monthDate, 'start of month'), ' ', 'T') || 'Z' as startDate,
+            REPLACE(DATETIME(monthDate, 'start of month', '+1 month', '-1 second'), ' ', 'T') || 'Z' as endDate
+          FROM MonthDates
+        ) AS months
+      ),
+      CumulativeTotals AS (
+        SELECT
+          startDate as date,
+          income,
+          expense,
+          (income - expense) as effect,
+          SUM(income - expense) OVER (ORDER BY startDate ASC) as networth
+        FROM MonthlyTotals
       )
       SELECT
-        months.startDate as month,
-        COALESCE((
-          SELECT
-            SUM(
-              CASE
-                WHEN ac.type IN ('asset', 'liability') AND ad.type IN ('income', 'expense') THEN amount
-                WHEN ad.type IN ('asset', 'liability') AND ac.type IN ('income', 'expense') THEN -amount
-              END
-            ) as delta
-          FROM entries AS e
-          LEFT JOIN accounts AS ac ON e.credit_account_id = ac.id
-          LEFT JOIN accounts AS ad ON e.debit_account_id = ad.id
-          WHERE
-            e.date BETWEEN months.startDate AND months.endDate
-        ), 0) as effect
-      FROM (
-        SELECT REPLACE(DATETIME(monthDate, 'start of month'), ' ', 'T') || 'Z' as startDate,
-          REPLACE(DATETIME(monthDate, 'start of month', '+1 month', '-1 second'), ' ', 'T') || 'Z' as endDate
-        FROM MonthDates
-      ) AS months;
+        date,
+        effect,
+        expense,
+        income,
+        networth
+      FROM CumulativeTotals;
     ''');
 
-    return result.map((row) {
-      return {'month': row['month'], 'savings': row['savings']};
-    }).toList();
+    return results.map((json) => Month.fromJson(json)).toList();
+  }
+}
+
+class Month {
+  DateTime? date;
+  num? effect;
+  num? expense;
+  num? income;
+  num? networth;
+
+  Month({
+    this.date,
+    this.effect,
+    this.expense,
+    this.income,
+    this.networth,
+  });
+
+  factory Month.fromJson(Map<String, dynamic> json) {
+    return Month(
+      date: DateTime.tryParse(json['date']),
+      effect: json['effect'] / 100,
+      expense: json['expense'] / 100,
+      income: json['income'] / 100,
+      networth: json['networth'] / 100,
+    );
   }
 }
